222909df4fa699ab195f5fb42813813a583c350d
Florian Lackner <florian.lackner@gmail.com>
2018-09-23 21:20:48 +0200

    add stub method littlewing fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 extern crate littlewing;
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn get_next_move(board_fen: &str, white: bool) -> String {
+    "e7".into()
+}
